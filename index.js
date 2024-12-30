15a0c2bb478a5f868d865bc6134b38f6d850d17e
Saransh Bari <131943503+SaranshPrasad@users.noreply.github.com>
2024-12-30 08:20:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 7777;
 // middlewares for frontend 
 app.use(cors({
-    origin: 'https://matched-livid.vercel.app', 
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization']
+    credentials: true
   }));
 
 // middlewares 
